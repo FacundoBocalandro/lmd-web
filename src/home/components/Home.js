87f4c14ce99299fb120a07b9e0e662571f3522ae
nicolefox <fox_nicole@hotmail.com>
2021-05-30 10:24:03 -0300

    history in home removed

--- src/home/components/Home.js
+++ src/home/components/Home.js
@@ -2,7 +2,6 @@ import {useHistory} from "react-router";
 import Navbar from "../../navbar/components/Navbar";
 
 const Home = ({logout}) => {
-    const history = useHistory();
 
     return (
         <Navbar logout={logout}> </Navbar>
