bbcba4e7ac4950a0a0864135d9134e0878ea5e46
facundobocalandro <facundo@bocalandro.com.ar>
2021-08-30 00:18:39 -0400

    Register doctor

--- src/session/session.services.js
+++ src/session/session.services.js
@@ -1,7 +1,8 @@
 import {post, get} from "../utils/http";
 
 export const services = {
-    registerUser: (user) => post('users/register', user, {noAuth: true}),
+    registerPatient: (user) => post('users/register', user, {noAuth: true}),
+    registerDoctor: (user) => post('users/register/doctor', user, {noAuth: true}),
     checkUsernameUsed: (username) => get(`users/available/${username}`, {noAuth: true}),
     login: (form) => post('login', form, {noAuth: true}),
     getUserInfo: (token) => get('users/current', {token})
